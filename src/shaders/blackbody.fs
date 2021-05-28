65b7aeb7e205795b875fecd9e00ef3bf05c9e57a
HollowaySean <hollowayseanm@gmail.com>
2021-05-27 18:52:10 -0700

    Tweaks

--- src/shaders/blackbody.fs
+++ src/shaders/blackbody.fs
@@ -6,18 +6,22 @@ in vec2 TexCoord;
 
 uniform sampler2D densTex;
 uniform sampler2D tempTex;
-uniform float bMod = 500.0;
+uniform float bMod = 0.00000000000005;
 uniform float sbConstant = 1.0;
 
 void main()
 {
     float temp = texture(tempTex, TexCoord).x;
-    float intensity = bMod * sbConstant * temp * temp * temp * temp;
+    float dens = texture(densTex, TexCoord).x;
+    dens = clamp(dens, 0.0, 1.0);
+    float intensity = dens * bMod * sbConstant * temp * temp * temp * temp;
     intensity = min(intensity, 1.0f);
 
+    float t = (temp - 1900.0) / (5200.0 - 1900.0);
+
     float red = 1.0f;
-    float blue = temp * (1.0 - 0.5765) + 0.5765;
-    float green = temp * (1.0 - 0.1608) + 0.1608;
+    float blue = t * (1.0 - 0.5765) + 0.5765;
+    float green = t * (1.0 - 0.1608) + 0.1608;
 
     FragColor = intensity * vec4(red, blue, green, 1.0);
 }

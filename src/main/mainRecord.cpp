2a8a0a98545f32d67d3bd43b1f6fab9d2ba07ba0
HollowaySean <hollowayseanm@gmail.com>
2021-06-01 10:19:22 -0700

    Video generation

--- src/main/mainRecord.cpp
+++ src/main/mainRecord.cpp
@@ -8,6 +8,10 @@
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
+#include <EasyBMP/EasyBMP.h>
+#include <EasyBMP/EasyBMP_BMP.h>
+#include <EasyBMP/EasyBMP_DataStructures.h>
+#include <EasyBMP/EasyBMP_VariousBMPutilities.h>
 
 using namespace ImGui;
 
@@ -23,25 +27,37 @@ std::string projectPath;
 
 int main(int argc, char** argv){
 
+    // Recording properties
+    int resolution = 200;
+    int winWidth = 800;
+    float fps = 60.0;
+    float timeStep = 1.0 / fps;
+    int numFrames = 1200;
+    std::string filename = "test";
+
     // Get project path
     std::string fullpath = argv[0];
     projectPath = fullpath.substr(0, fullpath.find_last_of("/")) + "/..";
 
     // Initialize state objects
-    WindowProps props;
-    LoadWindow("match", &props);
-    SimState state(10);
+    // WindowProps props;
+    // LoadWindow("record", &props);
+    SimState state(resolution);
     SimSource sources(&state);
-    LoadState("match", &state, &sources);
+    LoadState("record", &state, &sources);
 
     // Set up simulation and control windows
-    GLFWwindow* window = SimWindowSetup(10, props.winWidth);
-    ControlWindowSetup(window, props.controlWidth);
+    GLFWwindow* window = SimWindowSetup(resolution, winWidth);
 
     // Initialize timers
-    SimTimer timer(props.maxFrameRate);
-    timer.TrackFrameRatePerMS(1000);
+    SimTimer timer(fps);
     timer.StartSimulation();
+
+    // Create array for image data
+    BMP output;
+    output.SetSize(winWidth, winWidth);
+    output.SetBitDepth(8);
+    unsigned char* imageData = (unsigned char *)malloc((int)(winWidth*winWidth*3));
 
     // Simulation loop
     while(!glfwWindowShouldClose(window))
@@ -51,21 +67,43 @@ int main(int argc, char** argv){
 
         // Draw current density to OpenGL window
         SimWindowRenderLoop(window, state.fields.dens, state.fields.temp);
-
-        // Draw control window
-        ControlWindowRenderLoop(window, &state, &sources, &timer, &props);
+        glfwSwapBuffers(window);
 
         // Update dynamic sources
         sources.UpdateSourcesDynamic();
 
         // Update simulation state
-        state.SimulationStep(timer.DeltaTime());
+        state.SimulationStep(timeStep);
 
-        // Sleep until frame is complete
-        timer.EndFrame();
+        // Save openGl data to images
+        glReadPixels(0, 0, winWidth, winWidth, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+        for(int i = 0; i < winWidth; i++){
+            for(int j = 0; j < winWidth; j++){
+
+                int ind = 3 * ((i * winWidth) + j);
+                RGBApixel pixelColor;
+                pixelColor.Red      = imageData[ind];
+                pixelColor.Green    = imageData[ind+1];
+                pixelColor.Blue     = imageData[ind+2];
+
+                *output(j, winWidth - (i + 1)) = pixelColor;
+            }
+        }
+        std::string savePath = projectPath + "/output/bmp/" + filename + "_" + std::to_string(timer.CurrentFrame()) + ".bmp";
+        output.WriteToFile(const_cast<char *>(savePath.c_str()));
+
+        // Read out frame rate per every 
+        timer.DisplayFrameRate(int(fps));
+
+        // Escape once complete
+        if(timer.CurrentFrame() >= numFrames){
+            break;
+        }
     }
 
-    CloseWindows(window);
+    // End GLFW context
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     // Exit code
     return 0;
